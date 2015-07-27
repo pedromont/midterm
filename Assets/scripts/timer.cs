aceec0098874b8665973c5ce8a62ed6280798333
Pedro Mont <pm2558@nyu.com>
2015-07-27 00:04:31 -0400

    Working on winning scripts

---
+++ Assets/scripts/timer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour {
+
+	public bool inwincube;
+	public Text myTextObject; // assign in inspector
+	float timeElapsed = 10f;
+
+	void Start(){
+		inwincube = false;
+	}
+	// Update is called once per frame
+	void Update () {
+
+		timeElapsed -= Time.deltaTime;
+
+		if (timeElapsed > 0 && inwincube == false) {
+			// display current time elapsed; convert number to a string
+			myTextObject.text = "Time before the gate closes: " + timeElapsed.ToString ();
+		} else if (timeElapsed>0 && inwincube ==true){
+			myTextObject.text = "You managed to board your flight \n YOU WIN";
+		} else {
+			myTextObject.text = "You lost your flight!! \n GAME OVER";
+		}
+	}
+
+}
---
+++ Assets/scripts/wingame.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class wingame : MonoBehaviour {
+
+	//public bool win;
+	public ParticleSystem fireParticles2; // assign in Inspector
+
+	void Start(){
+	//	win = false;
+	}
+	// if the player enters this trigger, delete the player from the game
+	void OnTriggerEnter ( Collider activator ) {
+		// to delete the whole thing, you must delete the gameobject
+		//Destroy ( activator.gameObject );
+
+		inwincube = true;
+		fireParticles2.Play (); // and now the fire will begin
+
+	}
+}
